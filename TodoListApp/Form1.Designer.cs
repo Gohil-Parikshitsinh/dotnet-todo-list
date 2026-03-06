75f54fc0c8231d7bcdf11e7b6d4ebcd9917e7851
Gohil-Parikshitsinh <parikshitsinhgohil03@gmail.com>
2026-03-06 08:36:36 +0530

    Initial commit - Todo List Windows Forms App

---
+++ TodoListApp/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace TodoListApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTaskName = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.taskGrid = new System.Windows.Forms.DataGridView();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblDone = new System.Windows.Forms.Label();
+            this.lblPending = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.taskGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Description";
+            // 
+            // txtTaskName
+            // 
+            this.txtTaskName.Location = new System.Drawing.Point(191, 26);
+            this.txtTaskName.Name = "txtTaskName";
+            this.txtTaskName.Size = new System.Drawing.Size(332, 22);
+            this.txtTaskName.TabIndex = 2;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(191, 56);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(332, 22);
+            this.txtDescription.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(65, 99);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(119, 43);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add Task";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(230, 99);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(119, 43);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update Task";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(404, 99);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(119, 43);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete Task";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnDone
+            // 
+            this.btnDone.Location = new System.Drawing.Point(576, 99);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(119, 43);
+            this.btnDone.TabIndex = 7;
+            this.btnDone.Text = "Mark Done";
+            this.btnDone.UseVisualStyleBackColor = true;
+            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // taskGrid
+            // 
+            this.taskGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.taskGrid.Location = new System.Drawing.Point(65, 172);
+            this.taskGrid.Name = "taskGrid";
+            this.taskGrid.RowHeadersWidth = 51;
+            this.taskGrid.RowTemplate.Height = 24;
+            this.taskGrid.Size = new System.Drawing.Size(630, 266);
+            this.taskGrid.TabIndex = 8;
+            this.taskGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.taskGrid_CellClick);
+            this.taskGrid.RowPrePaint += new System.Windows.Forms.DataGridViewRowPrePaintEventHandler(this.taskGrid_RowPrePaint_1);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(576, 28);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(82, 16);
+            this.lblTotal.TabIndex = 9;
+            this.lblTotal.Text = "Total Tasks:";
+            // 
+            // lblDone
+            // 
+            this.lblDone.AutoSize = true;
+            this.lblDone.Location = new System.Drawing.Point(579, 48);
+            this.lblDone.Name = "lblDone";
+            this.lblDone.Size = new System.Drawing.Size(73, 16);
+            this.lblDone.TabIndex = 10;
+            this.lblDone.Text = "Completed";
+            // 
+            // lblPending
+            // 
+            this.lblPending.AutoSize = true;
+            this.lblPending.Location = new System.Drawing.Point(582, 68);
+            this.lblPending.Name = "lblPending";
+            this.lblPending.Size = new System.Drawing.Size(57, 16);
+            this.lblPending.TabIndex = 11;
+            this.lblPending.Text = "Pending";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPending);
+            this.Controls.Add(this.lblDone);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.taskGrid);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtTaskName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.taskGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTaskName;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.DataGridView taskGrid;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblDone;
+        private System.Windows.Forms.Label lblPending;
+    }
+}
+
---
+++ TodoListApp/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TodoListApp
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection(
+@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TodoDB;Integrated Security=True");
+
+        int selectedTaskId = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadTasks();
+        }
+
+        void LoadTasks()
+        {
+            conn.Open();
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Tasks", conn);
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+
+            taskGrid.DataSource = dt;
+
+            conn.Close();
+
+            UpdateTaskCounter();
+        }
+
+        void UpdateTaskCounter()
+        {
+            int total = taskGrid.Rows.Count;
+            int done = 0;
+            int pending = 0;
+
+            foreach (DataGridViewRow row in taskGrid.Rows)
+            {
+                if (row.Cells["Status"].Value != null)
+                {
+                    string status = row.Cells["Status"].Value.ToString();
+
+                    if (status == "Done")
+                        done++;
+                    else
+                        pending++;
+                }
+            }
+
+            lblTotal.Text = "Total Tasks: " + total;
+            lblDone.Text = "Completed: " + done;
+            lblPending.Text = "Pending: " + pending;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            string query = "INSERT INTO Tasks (Title, Description, Status) VALUES (@title, @desc, 'Pending')";
+
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            cmd.Parameters.AddWithValue("@title", txtTaskName.Text);
+            cmd.Parameters.AddWithValue("@desc", txtDescription.Text);
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            MessageBox.Show("Task Added Successfully!");
+
+            LoadTasks();
+        }
+
+        private void taskGrid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = taskGrid.Rows[e.RowIndex];
+
+                selectedTaskId = Convert.ToInt32(row.Cells["TaskID"].Value);
+
+                txtTaskName.Text = row.Cells["Title"].Value.ToString();
+                txtDescription.Text = row.Cells["Description"].Value.ToString();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            string query = "DELETE FROM Tasks WHERE TaskID=@id";
+
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            cmd.Parameters.AddWithValue("@id", selectedTaskId);
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            MessageBox.Show("Task Deleted!");
+
+            LoadTasks();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            string query = "UPDATE Tasks SET Title=@title, Description=@desc WHERE TaskID=@id";
+
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            cmd.Parameters.AddWithValue("@title", txtTaskName.Text);
+            cmd.Parameters.AddWithValue("@desc", txtDescription.Text);
+            cmd.Parameters.AddWithValue("@id", selectedTaskId);
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            MessageBox.Show("Task Updated!");
+
+            LoadTasks();
+        }
+
+        private void btnDone_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            string query = "UPDATE Tasks SET Status='Done' WHERE TaskID=@id";
+
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            cmd.Parameters.AddWithValue("@id", selectedTaskId);
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            MessageBox.Show("Task Marked as Done!");
+
+            LoadTasks();
+        }
+
+        private void taskGrid_RowPrePaint_1(object sender, DataGridViewRowPrePaintEventArgs e)
+        {
+            if (taskGrid.Rows[e.RowIndex].Cells["Status"].Value != null)
+            {
+                string status = taskGrid.Rows[e.RowIndex].Cells["Status"].Value.ToString();
+
+                if (status == "Done")
+                {
+                    taskGrid.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.LightGreen;
+                }
+            }
+        }
+    }
+}
